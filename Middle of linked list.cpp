615eab59b674eb95aa4b60807f13ff55a294522b
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-02-05 13:31:27 +0530

    Update Middle of linked list.cpp

--- Middle of linked list.cpp
+++ Middle of linked list.cpp
@@ -25,3 +25,32 @@ class Solution{
          return value;
        }
 };
+
+
+// fast ans slow pointer approach
+
+class Solution{
+    public:
+    
+    /* Should return data of middle node. If linked list is empty, then  -1*/
+    int getMiddle(Node *head)
+    {
+        // Your code here
+          Node* slow=head;
+          Node* fast=head->next;
+
+          if(head==NULL || head->next==NULL)
+          return head->data;
+
+          while(fast!=NULL) {
+               fast=fast->next;
+               if(fast!=NULL) {
+               fast=fast->next;
+            }
+      
+            slow = slow->next;
+           }
+
+        return slow->data;
+    }
+};
