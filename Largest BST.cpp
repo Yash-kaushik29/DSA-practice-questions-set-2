497ac5092ab8793004491a03f4e3c180f7db204a
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-03-05 19:13:31 +0530

    Create Largest BST.cpp

---
+++ Largest BST.cpp
@@ -0,0 +1,47 @@
+class Solution{
+    public:
+    
+    class info {
+       public:
+         int maxi;
+         int mini;
+         bool isBST;
+         int size;
+    };
+
+    info solve(Node* root, int& ans) {
+    
+        if(root == NULL) {
+           return {INT_MIN, INT_MAX, true, 0};
+        }
+    
+        info left = solve(root->left, ans);
+        info right = solve(root->right, ans);
+        
+        info currNode;
+        
+        currNode.size = left.size + right.size + 1;
+        currNode.maxi = max(root->data, right.maxi);
+        currNode.mini = min(root->data, left.mini);
+        
+        if(left.isBST && right.isBST && (root->data > left.maxi && root->data < right.mini)) {
+            currNode.isBST = true;
+            ans = max(ans, currNode.size);
+        }
+        else {
+            currNode.isBST = false;
+        }
+        
+        return currNode;
+    }
+
+
+    
+    int largestBst(Node *root)
+    {
+    	//Your code here
+    	int maxSize = 0;
+    	info temp = solve(root, maxSize);
+    	return maxSize;
+    }
+};
