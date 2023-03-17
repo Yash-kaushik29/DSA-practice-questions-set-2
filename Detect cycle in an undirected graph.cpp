bc6c39e49caa293ebd4a781628e6ad886c698ff4
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-03-17 09:38:48 +0530

    Update Detect cycle in an undirected graph.cpp

--- Detect cycle in an undirected graph.cpp
+++ Detect cycle in an undirected graph.cpp
@@ -1,3 +1,5 @@
+// Using Breadth first search(BFS)
+
 class Solution {
   public:
     bool detect(int src,vector<int> adj[],int vis[])
@@ -34,6 +36,43 @@ class Solution {
             if(!vis[i])
             {
                 if(detect(i,adj,vis))
+                    return true;
+            }
+        }
+        return false;
+    }
+};
+
+// Using Depth first search(DFS)
+
+class Solution {
+  public:
+    bool detect(int src, int parent, vector<int> adj[], int vis[])
+    {
+        vis[src]=1;
+        
+        for(auto node: adj[src]) {
+            if(!vis[node]) {
+                if(detect(node, src, adj, vis)) return true;
+            }
+            
+            else if(vis[node] && parent!=node) {
+                return true;
+            }
+        }
+            
+        return false;
+    }
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(int V, vector<int> adj[]) {
+        // Code here
+        int vis[V]={0};
+        
+        for(int i=0;i<V;i++)
+        {
+            if(!vis[i])
+            {
+                if(detect(i,-1,adj,vis))
                     return true;
             }
         }
