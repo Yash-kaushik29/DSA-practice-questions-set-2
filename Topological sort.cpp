742aea3520d6486c0ba26f5ed54ba844c38166be
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-03-19 11:33:08 +0530

    Update Topological sort.cpp

--- Topological sort.cpp
+++ Topological sort.cpp
@@ -1,3 +1,46 @@
+// Using BFS(Kahn's Algorithm)
+
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    vector<int> indegree(V,0);
+	    
+	    for(int i=0; i<V; i++) {
+	        for(auto j: adj[i]) {
+	            indegree[j]++;
+	        }
+	    }
+	    
+	    queue<int> q;
+	    for(int i=0; i<V; i++) {
+	        if(indegree[i] == 0)
+	          q.push(i);
+	    }
+	    
+	    vector<int> ans;
+	    
+	    while(!q.empty()) {
+	        int front=q.front();
+	        q.pop();
+	        
+	        ans.push_back(front);
+	        
+	        for(auto neighbour: adj[front]) {
+	            indegree[neighbour]--;
+	            if(indegree[neighbour] == 0)
+	                q.push(neighbour);
+	        }
+	    }
+	    
+	    return ans;
+	}
+};
+
+// Using DFS
+
 class Solution
 {
 	public:
