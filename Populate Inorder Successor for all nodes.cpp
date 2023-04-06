22fe9716cce861cf84f4c9a7d1727e2f8dca82b5
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-04-06 20:28:34 +0530

    Create Populate Inorder Successor for all nodes.cpp

---
+++ Populate Inorder Successor for all nodes.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    void solve(Node* root, Node* &prev) {
+        if(!root) return ;
+        
+        solve(root->left, prev);
+        
+        if(prev) {
+            prev->next = root;
+        }
+        prev=root;
+        
+        solve(root->right, prev);
+    }
+
+
+    void populateNext(Node *root)
+    {
+        Node* prev=NULL;
+        solve(root, prev);
+    }
+};
