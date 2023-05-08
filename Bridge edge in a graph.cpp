b08b619584748ebaac1ef8eaf55f1caa5e4d7be3
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-05-08 11:11:46 +0530

    Update Bridge edge in a graph.cpp

--- Bridge edge in a graph.cpp
+++ Bridge edge in a graph.cpp
@@ -1,45 +1,47 @@
 class Solution
 {
-	bool isBridgeFlag=false;
-    void dfs(int node,int &timer,int parent,vector<int> &disc,vector<int> &low,vector<bool> &vis,vector<int> adj[],int c,int d){
-        vis[node]=true;
-        disc[node]=timer;
-        low[node]=timer;
-        timer++;
-        for(int i=0;i<adj[node].size();i++){
-            int nbr=adj[node][i];
-            if(nbr==parent){
+    bool isBridgeFlag=false;
+	
+    void dfs(int node, int& timer, int parent, vector<int>& disc, vector<int>& low,
+                   vector<bool>& vis, vector<int> adj[], int c, int d) {
+        vis[node] = true;
+        low[node] = disc[node] = timer++;
+        
+        for(auto nbr: adj[node]) {
+            if(nbr == parent) {
                 continue;
             }
-            if(vis[nbr]){
-                low[node]=min(low[node],disc[nbr]);
-            }
-            else{
-                dfs(nbr,timer,node,disc,low,vis,adj,c,d);
-                low[node]=min(low[node],low[nbr]);
-                if(low[nbr]>disc[node]){
-                    // cout<<nbr<<" "<<node<<endl;
-                    if((nbr==c && node==d) || (nbr==d && node==c)){
-                        isBridgeFlag=true;
+            if(!vis[nbr]) {
+                dfs(nbr, timer, node, disc, low, vis, adj, c, d);
+                low[node] = min(low[node], low[nbr]);
+                
+                if(low[nbr] > disc[node]) {
+                    if((nbr==c && node==d) || (nbr==d && node==c)) {
+                        isBridgeFlag = true;
                     }
                 }
+            }
+            else {
+                low[node] = min(low[node], low[nbr]);
             }
         }
     }
 	public:
-    //Function to find if the given edge is a bridge in graph.
+    
     int isBridge(int V, vector<int> adj[], int c, int d) {
-        isBridgeFlag=false;
+        
         vector<int> disc(V,-1);
         vector<int> low(V,-1);
         vector<bool> vis(V,false);
         int timer=0;
+        
         for(int i=0;i<V;i++){
             if(vis[i]){
                 continue;
             }
-            dfs(i,timer,-1,disc,low,vis,adj,c,d);
+            dfs(i, timer, -1, disc, low, vis, adj, c, d);
         }
+        
         return isBridgeFlag;  
     }
 };
