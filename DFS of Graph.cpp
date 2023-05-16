698c2325a3fd19ec4bc1ab424ae991d4191a7b62
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-05-16 12:14:27 +0530

    Update DFS of Graph.cpp

--- DFS of Graph.cpp
+++ DFS of Graph.cpp
@@ -1,16 +1,13 @@
 class Solution {
   public:
-    // Function to return a list containing the DFS traversal of the graph.
-    void DFS(int source, int V, vector<int> adj[], vector<int>& dfs, vector<int>& vis) {
+    void DFS(int source, vector<int> adj[], vector<int>& dfs, vector<int>& vis) {
         
-        vis[source]=1;
+        vis[source] = 1;
         dfs.push_back(source);
         
-        for(int i=0; i<adj[source].size(); i++) {
-            int neighbour = adj[source][i];
-            
-            if(!vis[neighbour]) {
-                DFS(neighbour, V, adj, dfs, vis);
+        for(auto nbr: adj[source]) {
+            if(!vis[nbr]) {
+                DFS(nbr, adj, dfs, vis);
             }
         }
     }
@@ -20,8 +17,9 @@ class Solution {
         vector<int> dfs;
         vector<int> vis(V,0);
         
-        DFS(0, V, adj, dfs, vis);
+        DFS(0, adj, dfs, vis);
         
         return dfs;
+    }
     }
 };
