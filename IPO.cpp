fbbcf9d6495c6b0a809de4d77f6c157c5dc0937d
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-05-22 23:40:24 +0530

    Update IPO.cpp

--- IPO.cpp
+++ IPO.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
     static bool cmp(pair<int, int>& a, pair<int, int>& b) {
-        if (a.second != b.second)
+        if(a.second != b.second) {
             return a.second < b.second;
+        }
         return a.first < b.first;
     }
 
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
         
         int n = profits.size();
-        vector<pair<int, int>> projects(n);
-        for (int i = 0; i < n; i++)
-            projects[i] = {profits[i], capital[i]};
-        sort(projects.begin(), projects.end(), cmp);
-        int i = 0;
-        priority_queue<int> PQ;
-        while (k--) {
-            while (i < n && projects[i].second <= w) {
-                PQ.push(projects[i].first);
+        vector<pair<int, int> > project(n);
+
+        for(int i=0; i<n; i++) {
+            project[i] = {profits[i], capital[i]};
+        }
+
+        sort(project.begin(), project.end(), cmp);
+
+        int i=0;
+        priority_queue<int> pq;
+
+        while(k--) {
+            while(i<n && project[i].second<=w) {
+                pq.push(project[i].first);
                 i++;
             }
-            if (!PQ.empty()) {
-                w += PQ.top();
-                PQ.pop();
+
+            if(!pq.empty()) {
+                w+=pq.top();
+                pq.pop();
             }
         }
+
         return w;
     }
 };
