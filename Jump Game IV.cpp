7bfb05440f3b4981a7f58fc7391d0f7f11b776cf
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-05-25 00:40:53 +0530

    Update Jump Game IV.cpp

--- Jump Game IV.cpp
+++ Jump Game IV.cpp
@@ -2,28 +2,41 @@ class Solution {
 public:
     int minJumps(vector<int>& arr) {
         int n = arr.size();
-        unordered_map<int, vector<int>> indicesOfValue;
-        for (int i = 0; i < n; i++)
-            indicesOfValue[arr[i]].push_back(i);
-        vector<bool> visited(n); visited[0] = true;
-        queue<int> q; q.push(0);
-        int step = 0;
-        while (!q.empty()) {
-            for (int size = q.size(); size > 0; --size) {
-                int i = q.front(); q.pop();
-                if (i == n - 1) return step; // Reached to last index
-                vector<int>& next = indicesOfValue[arr[i]];
-                next.push_back(i - 1); next.push_back(i + 1);
-                for (int j : next) {
-                    if (j >= 0 && j < n && !visited[j]) {
-                        visited[j] = true;
+        unordered_map<int, vector<int>> index;
+
+        for(int i=0; i<n; i++) {
+            index[arr[i]].push_back(i);
+        }
+        vector<bool> vis(n, false);
+        queue<int> q;
+
+        q.push(0);
+        vis[0]=true;
+        int steps=0;
+
+        while(!q.empty()) {
+            int size = q.size();
+            while(size--) {
+                int node = q.front();
+                q.pop();
+
+                if(node == n-1) return steps;
+
+                vector<int> next = index[arr[node]];
+                next.push_back(node-1);
+                next.push_back(node+1);
+
+                for(int j: next) {
+                    if(j>=0 && j<n && !vis[j]) {
                         q.push(j);
+                        vis[j]=true;
                     }
                 }
-                indicesOfValue[arr[i]].clear(); // avoid later lookup indicesOfValue arr[i]
+                // avoid later lookup indicesOfValue arr[i]
+                index[arr[node]].clear();
             }
-            step++;
+            steps++;
         }
-        return 0;
+        return -1;
     }
 };
