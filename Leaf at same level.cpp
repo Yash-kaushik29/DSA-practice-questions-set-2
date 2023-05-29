c45f21d5f1beb1062ccebe4aa9c4cb43dd427c93
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-05-29 21:25:42 +0530

    Update Leaf at same level.cpp

--- Leaf at same level.cpp
+++ Leaf at same level.cpp
@@ -1,16 +1,18 @@
 class Solution{
   public:
-    /*You are required to complete this method*/
     void solve(Node* root, int h, int& l, bool& ans) {
-        if(root==NULL) return ;
-        if(ans==0) return ;
+        if(root == NULL) return ;
+        if(ans == 0) return ;
         
-        if(!root->left and !root->right) {
-            if(l==-1) l=h;
-            else {
-                if(h!=l) ans=0; 
+        if(!root->left && !root->right) {
+            //For first leaf node 
+            if(l==-1) {
+                l=h;
             }
-            return ;
+            // for further leaf nodes, check for same level
+            else if(l!=h) {
+                ans=0;
+            }
         }
         solve(root->left, h+1, l, ans);
         solve(root->right, h+1, l, ans);
