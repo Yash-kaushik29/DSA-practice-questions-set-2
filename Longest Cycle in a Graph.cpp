bf583d9191a04a4dbbf949ba7244fe65940388d3
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-06-01 22:57:55 +0530

    Update Longest Cycle in a Graph.cpp

--- Longest Cycle in a Graph.cpp
+++ Longest Cycle in a Graph.cpp
@@ -1,45 +1,58 @@
 class Solution {
 public:
     void topo(int i, vector<vector<int>>& adj, vector<int> &vis, stack<int> &st){
-	    vis[i]=1;
-	    for(auto x: adj[i])
-	        if(vis[x]==0)
-	            topo(x,adj,vis,st);
-	    st.push(i);
+	vis[i]=1;
+
+        for(auto nbr: adj[i]) {
+            if(!vis[nbr]) {
+                topo(nbr, adj, vis, st);
+            }
+        }
+        st.push(i);
 	}
 
-	void dfs(int i, vector<int> adj[], vector<int> &vis, vector<int> &ans){
-	    vis[i]=1;
-        ans.push_back(i);
-	    for(auto x: adj[i])
-	        if(vis[x]==0)
-	            dfs(x, adj, vis, ans);
-	}
+    void dfs(int i, vector<int> adj[], vector<int> &vis, vector<int> &temp){
+        vis[i]=1;
+        temp.push_back(i);
+
+        for(auto nbr: adj[i]) {
+            if(!vis[nbr]) {
+                dfs(nbr, adj, vis, temp);
+            }
+        }
+    }
 
     int kosaraju(int n, vector<vector<int>>& adj) {
         int ans=0;
         stack<int> st;
-        vector<int> vis(n, 0);
-        for(int i=0;i<n;i++) {
-            if(vis[i]==0) {
+        vector<int> vis(n,0);
+
+        for(int i=0; i<n; i++) {
+            if(vis[i] == 0) {
                 topo(i, adj, vis, st);
             }
         }
+
         vector<int> adjRev[n];
-        for(int i=0;i<n;i++) {
+        for(int i=0; i<n; i++) {
             vis[i]=0;
-            for(auto x: adj[i]) {
-                adjRev[x].push_back(i);
+
+            for(auto nbr: adj[i]) {
+                adjRev[nbr].push_back(i);
             }
         }
-        while(st.size()) {
-            int node=st.top(); 
+
+        while(!st.empty()) {
+            int top = st.top();
             st.pop();
-            if(vis[node]==0) {
+
+            if(vis[top] == 0) {
                 vector<int> temp;
-                dfs(node, adjRev, vis, temp);           
-                if(ans<temp.size()) 
+                dfs(top, adjRev, vis, temp);
+
+                if(temp.size() > ans) {
                     ans=temp.size();
+                }
             }
         }
         return ans;
@@ -47,17 +60,16 @@ public:
 
     int longestCycle(vector<int>& a) {
 
-        int i, n=a.size();
-        vector<vector<int>>adj(n);
-        for(i=0;i<n;i++) {
-            if(a[i]>=0)
+        int n=a.size();
+        vector<vector<int>> adj(n);
+
+        for(int i=0; i<n; i++) {
+            if(a[i] >= 0)
              adj[i].push_back(a[i]);
         }
+
         int cyclesize=kosaraju(n, adj);
-        if(cyclesize>1){
-            return cyclesize;
-        }
-        return -1;
         
+        return cyclesize > 1 ? cyclesize : -1; 
     }
 };
