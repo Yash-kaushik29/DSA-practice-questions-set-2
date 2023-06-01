40bb5ad1a74e4b834a1d1551256bdee6364eeef9
Yash-kaushik29 <115134639+Yash-kaushik29@users.noreply.github.com>
2023-06-01 23:32:01 +0530

    Update Max Level Sum in Binary Tree.cpp

--- Max Level Sum in Binary Tree.cpp
+++ Max Level Sum in Binary Tree.cpp
@@ -2,27 +2,29 @@ class Solution{
   public:
     /*You are required to complete below method */
     int maxLevelSum(Node* root) {
-        // Your code here
-        int ans=INT_MIN;
-        int sum=0;
-        queue<pair<Node*,int>> q;
-        q.push({root,0});
-        int level=0;
-        while(!q.empty()){
-            Node* node=q.front().first;
-            int curr=q.front().second;
-            q.pop();
-            if(level==curr) {
-                sum+=node->data;
-            }else{
-                ans=max(ans,sum);
-                sum=node->data;
-                level=curr;
+        queue<Node*> q;
+        q.push(root);
+        int maxSum=INT_MIN;
+
+        while(!q.empty()) {
+            int size = q.size();
+            int currSum=0;
+
+            while(size--) {
+                Node* front = q.front();
+                q.pop();
+
+                currSum+=front->data;
+
+                if(front->left) q.push(front->left);
+                if(front->right) q.push(front->right);
             }
-            if(node->left) q.push({node->left,curr+1});
-            if(node->right) q.push({node->right,curr+1});
+            
+            if(currSum > maxSum) {
+                maxSum = currSum;
+            }
         }
-        ans=max(ans,sum);
-        return ans;
+        return maxSum;
     }
 };
+
